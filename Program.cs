0d2b6c8313c0b024e304a962a74d93b4f6b7f681
f03us <113976578+f03us@users.noreply.github.com>
2022-12-06 09:41:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+             Console.Write  ("Zadejte pocet radku: ");
+             int radky = int.Parse(Console.ReadLine());
+             Console.Write("Zadejte tloustku bunky: ");
+             int tloustka = int.Parse(Console.ReadLine());
+
+                if (radky == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Pocet radku s hodnotou nula neexsistuje");
+                   
+                }
+
+                if (tloustka == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tlostka nula neexsistuje");
+
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                for (int i = 0; i < radky; i++)
+                {
+                  for (int k = 0; k < tloustka; k++)
+                  {
+                      Console.Write("+");
+                  }
+
+                  for (int k = 0; k < tloustka; k++)
+                  {
+                      Console.Write("-");
+                  }
+
+                  for (int k = 0; k < tloustka; k++)
+                  {
+                      Console.Write("+");
+                  }
+
+                  for (int k = 0; k < tloustka; k++)
+                  {
+                      Console.Write("-");
+                  }
+
+                  for (int k = 0; k < tloustka; k++)
+                  {
+                      Console.Write("+");
+                  }
+
+                      Console.WriteLine();
+                   
+                }
+
+                Console.WriteLine();
+                if (radky == 'n')
+                {
+                    Console.WriteLine("Konec");
+                    break;
+
+                }
+
+                if (tloustka == 'n')
+                {
+                    Console.WriteLine("Konec");
+                    break;
+                }
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+           
+    
+}
